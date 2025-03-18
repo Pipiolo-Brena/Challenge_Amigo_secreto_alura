3e7742035d61dfee243de1a3246d8aae6f348196
Pipiolo-Brena <bjvictor18@gmail.com>
2025-03-17 19:44:26 -0600

    Metodo de sorteaAmigo()

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let nombresAmigos=[];
-let ganador;
+let amigoSecreto;
 
 function agregarAmigo(){
     let amigoAñadido = (document.getElementById('amigo').value);
@@ -20,6 +20,29 @@ function verAmigoNuevo(){
         let nuevoElemento=document.createElement("li");
         nuevoElemento.textContent=x;
         lista.appendChild(nuevoElemento);
+    }
+}
+
+function sortearAmigo(){
+    if(nombresAmigos.length>1){
+        let numeroAzar=Math.floor(Math.random()*nombresAmigos.length);
+        amigoSecreto= nombresAmigos[numeroAzar];
+
+        //Quitamos la lista de amigos
+        let listaAmgios=document.querySelector("#listaAmigos");
+        listaAmgios.innerHTML = ""
+
+        //Ponemos el ganador en pantalla
+        
+        let listaResultado=document.querySelector("#resultado");
+        let nuevoElemento=document.createElement("li");
+        nuevoElemento.textContent=`El amigo sorteado es: ${amigoSecreto}`;
+        listaResultado.appendChild(nuevoElemento);
+
+        //Vaciamos la lista de amigos
+        nombresAmigos=[];
+    }else{
+        alert("Amigos insuficientes para el sorteo, añadir más amigos.");
     }
 }
 
