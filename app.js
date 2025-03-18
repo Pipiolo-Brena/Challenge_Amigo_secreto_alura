3d2bb8672666fdc79f6ef1b62b164b27b285c853
Pipiolo-Brena <bjvictor18@gmail.com>
2025-03-17 18:57:06 -0600

    Nuevo metodo agregarAmigo() y declaracion de un array global para los nombres

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresAmigos=[];
+let ganador;
+
+function agregarAmigo(){
+    let amigoAñadido = (document.getElementById('amigo').value);
+    nombresAmigos.push(amigoAñadido);
+    verAmigoNuevo();
+    limpiarCaja();
+}
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
